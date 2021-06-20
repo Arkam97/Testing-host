d4624566d803b14188479f72819c19e41637bfd1
Arkam97 <husainmohamedarkam1997@gmail.com>
2021-06-20 21:11:35 +0530

    switched to new branch

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hi Im Arkam
+        </h1>
+        <h2>This is the change </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
